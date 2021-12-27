817866dee8dea8922ede232a8dafc71dca461624
jaehun <wognskec@hanyang.ac.kr>
2021-12-27 16:13:15 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Twitter Clone Coding</h1>
+
+- React JS
+- styled-component
+- FireBase
---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <ul>
+          <li>hello</li>
+          <li>world</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
