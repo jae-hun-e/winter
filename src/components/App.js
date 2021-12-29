94f0cdbeddfc0b5ec98ce061b07e04349325375a
jaehun <wognskec@hanyang.ac.kr>
2021-12-29 23:01:49 +0900

    logout 기능 추가 + react-router-dom version6 변경사항 확인하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
+
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
+import { authService } from "fbase";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <span>Profile</span>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
