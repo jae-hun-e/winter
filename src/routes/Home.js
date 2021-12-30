ec418eaf8bb528426a436a197f8eb3c14a30ea35
jaehun <wognskec@hanyang.ac.kr>
2021-12-30 22:45:22 +0900

    home.js에 twitt 기본 폼 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [twitt, setTwit] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setTwit(value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={twitt}
+        onChange={onChange}
+        type="text"
+        placeholder="뭐하는 중?"
+        maxlength={120}
+      />
+      <input type="submit" value="twitt" />
+    </form>
+  );
+};
 
 export default Home;
